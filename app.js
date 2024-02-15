2418e3a185398a33f52b9ebfba3b60350dd63eb1
Javen <javana.dz@gmail.com>
2024-02-15 12:25:04 +0700

    All done, tinggal deploy

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
 const express = require("express");
-const app = express();
 const sqlite = require("sqlite3").verbose();
-const db = new sqlite.Database("./cards.db", sqlite.OPEN_READWRITE, (err)=>{
-    if (err) return console.log("Gagal membuat database");
-    else return console.log("Berhasil membuat database");
-});
+const moment = require('moment-timezone');  
 const Hashids = require('hashids/cjs');
-const hashids = new Hashids('AIfhu934fb', 10);
 
+const app = express();
+const router = express.Router();
+const hashids = new Hashids('AIfhu934fb', 10);
+const db = new sqlite.Database("./cards.db", sqlite.OPEN_READWRITE, (err)=>{
+    if (err) return console.log("[!] Gagal connect database");
+    else return console.log(">> Berhasil connect database");
+});
+
+app.use(express.json());
 const PORT = 5000; 
 let sql;
 
-app.use(express.json());
+router.post('/post-card', (req, res)=>{
+    const date = moment().tz('Asia/Jakarta').format('DD-MM-YYYY HH:mm [WIB]');
+ 
+    try {
+        const {sender, recipient, message, theme} = req.body;
+        sql = "INSERT INTO cards(sender,recipient,message,theme) VALUES (?,?,?,?)"
 
-app.post('/post-card', (req, res)=>{
-    try{
-        const {sender, recipient, message} = req.body;
-        sql = "INSERT INTO cards(sender,recipient,message) VALUES (?,?,?)"
-
-        db.run(sql, [sender, recipient, message], (err)=>{
+        db.run(sql, [sender, recipient, message, theme], (err)=>{
             if(err) {
-                console.log("Gagal posting database", sender, recipient, message, err);
-                return res.json({
-                    status: 300,
+                console.log("[!] Gagal posting database", err);
+                return res.status(300).json({
                     success: false,
-                    error: "Gagal posting card"
+                    date: date,
+                    message: "Gagal posting card"
                 });
             }
 
+            console.log(">> Berhasil post database:\n", req.body)
             db.get("SELECT last_insert_rowid() as card_key", [], (err, row) => {
                 const hashedKey = hashids.encode(row.card_key); 
                 return res.status(200).json({
-                    status: 200,
                     success: true,
+                    date: date,
                     message: "Berhasil posting card",
                     card_key: hashedKey
                 });
@@ -40,13 +45,65 @@ app.post('/post-card', (req, res)=>{
         })
 
     } catch(error) {
-        return res.json({
-            status: 400, 
+        return res.status(400).json({
             success: false,
-            error: "Gagal post request"
+            message: "Gagal post request"
         });
     }
 })
 
+
+router.get('/get-card', (req, res)=>{
+    const date = moment().tz('Asia/Jakarta').format('DD-MM-YYYY HH:mm [WIB]');
+
+    try {
+        const {card_key} = req.query;
+        const decodedKey = parseInt(hashids.decode(card_key)[0]);
+        sql = "SELECT * FROM cards WHERE card_key = ?";
+
+        db.get(sql, [decodedKey], (err, row)=>{
+            if(err) {
+                console.log("[!] Gagal mengambil database");
+                return res.status(300).json({
+                    success: false,
+                    date: date,
+                    message: "Gagal mengambil card"
+                });
+            }
+
+            if(!row) {
+                console.log("[!] Database tidak ditemukan: ", decodedKey);
+                return res.status(404).json({
+                    success: false,
+                    date: date,
+                    message: "Database tidak ditemukan"
+                });
+            }
+
+            console.log(">> Berhasil get database:\n", row)
+            return res.status(200).json({
+                success: true,
+                date: date,
+                message: "Berhasil mengambil card",
+                card: {
+                    sender: row.sender,
+                    recipient: row.recipient,
+                    message: row.message,
+                    theme: row.theme,
+                }
+            });
+        })
+
+
+    } catch(error) {
+        return res.status(400).json({
+            success: false,
+            date: date,
+            message: "Gagal get request"
+        });
+    }
+})
+
+app.use('/api', router);
 app.listen(PORT);
 
--- table.js
+++ table.js
@@ -1,10 +1,22 @@
+//Kalau cards.db masi kosong, run "node table.js" 1x di terminal.
+
 const sqlite = require("sqlite3").verbose();
 const db = new sqlite.Database("./cards.db", sqlite.OPEN_READWRITE, (err)=>{
-    if (err) return console.log("Gagal membuat database");
+    if (err) return console.log("Gagal connect database");
+    else return console.log("Berhasil connect database");
 });
 
-const sql = "CREATE TABLE cards(card_key INTEGER PRIMARY KEY, sender TEXT, recipient TEXT, message TEXT)";
+const sql = `
+    CREATE TABLE cards (
+        card_key INTEGER PRIMARY KEY,
+        sender TEXT,
+        recipient TEXT,
+        message TEXT,
+        theme INTEGER
+    )
+`;
 
 db.run(sql, (err) => {
-    if (err) return console.error(err.message);
+    if (err) return console.log("[!] Gagal membuat database", err.message);
+    else return console.log(">> Berhasil membuat table");
 });
